36eb38364410b7c2e455237cd40f580252950d85
Albert Gil Escura <agilescura@gmail.com>
2025-04-08 20:06:07 +0200

    Add tap to exercises button

--- NavigationDoExercise/NavigationDoExercise/ContentView.swift
+++ NavigationDoExercise/NavigationDoExercise/ContentView.swift
@@ -1,11 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State var selectedTab = 0
+	
 	var body: some View {
-		TabView {
-			Tab("Exercises", systemImage: "figure.run") {
+		TabView(selection: $selectedTab) {
+			Tab("Exercises", systemImage: "figure.run", value: 0) {
+				Text("Exercises")
 			}
-			Tab("Settings", systemImage: "gear") {
+			Tab("Settings", systemImage: "gear", value: 1) {
+				Button("Tap to exercises") {
+					selectedTab = 0
+				}
 			}
 		}
 	}
