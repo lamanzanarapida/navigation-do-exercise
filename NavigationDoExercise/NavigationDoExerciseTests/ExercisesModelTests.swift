3126a36406500dfe55abd135266ebe60e2e1c594
Albert Gil Escura <agilescura@gmail.com>
2025-04-18 09:36:52 +0200

    Update tests

--- NavigationDoExercise/NavigationDoExerciseTests/ExercisesModelTests.swift
+++ NavigationDoExercise/NavigationDoExerciseTests/ExercisesModelTests.swift
@@ -2,15 +2,33 @@ import Testing
 @testable import NavigationDoExercise
 
 struct NavigationDoExercisesListTests {
-		@Test func givenExercisesWhenOnDeleteExercisesCalledThenExercisesDeleted() async throws {
-			let model = ExercisesModel(
-				exercises: [
-					.fake(.random)
-				]
-			)
-			
-			model.onDeleteExercises([0])
-			
-			#expect(model.exercises.isEmpty)
-		}
+	@Test func givenExercisesWhenOnAlertSwipeCalledThenAlertShownAndExercisesDeleted() async throws {
+		let exercise = Exercise.fake(.random)
+		
+		let model = ExercisesModel(
+			exercises: [
+				exercise
+			]
+		)
+		
+		model.alertButtonTapped(exercise)
+		model.confirmAlertButtonTapped()
+		
+		#expect(model.exercises.isEmpty)
+	}
+	
+	@Test func givenExercisesWhenOnDialogSwipeCalledThenDialogShownAndExerciseChangedToRunning() async throws {
+		let exercise = Exercise.fake(type: .cycling)
+		
+		let model = ExercisesModel(
+			exercises: [
+				exercise
+			]
+		)
+		
+		model.dialogButtonTapped(exercise)
+		model.confirmDialogButtonTapped(type: .running)
+		
+		#expect(model.exercises[0].type == .running)
+	}
 }
